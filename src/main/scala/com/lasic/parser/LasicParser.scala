5924e0841bd959db7987d0d0a4da14c2681d4f51
lmonson <lmonson@localhost>
2010-05-06 22:20:50 -0600

    Added empty, initial parser file

---
+++ src/main/scala/com/lasic/parser/LasicParser.scala
@@ -0,0 +1,11 @@
+package com.lasic.parser
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lmonson
+ * Date: May 6, 2010
+ * Time: 10:20:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object LasicParser 
